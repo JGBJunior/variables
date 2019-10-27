76ad1b613762aabb3d0c0eed0815ee94dfdc470a
JGBJunior <jgbjunior99@gmail.com>
2019-10-27 23:57:37 +0100

    <variables>

---
+++ unity/Assets/variables.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour {
+	string s1 = "nombre";
+	string s2 = "profesion";
+		string s3 = "aficion";
+		class edad;
+		class hijos;
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.log ("Juan jose");
+		Debug.log ("basurero");
+		Debug.log ("canicas");
+		Debug.log ("34");
+		Debug.log ("cero");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
